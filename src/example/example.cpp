18c3580debbc7249b1e29eb1c608ec70afb511bf
邹吉华 <ghuazo@live.com>
2023-07-17 21:32:13 +0800

    修改启动

--- src/example/example.cpp
+++ src/example/example.cpp
@@ -38,10 +38,16 @@ void start_evaluate(const char* account_config, const code_t& code, const std::v
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::vector<uint32_t> trading_days{ 20220801, 20220802, 20220803, 20220804, 20220805 };
-	start_evaluate("evaluate.ini", "SHFE.rb2210", trading_days);
-	//start_runtime("runtime.ini", "SHFE.rb2310");
+	if (argc > 1)
+	{
+		start_runtime("runtime.ini", "SHFE.rb2310");
+	}
+	else 
+	{
+		std::vector<uint32_t> trading_days{ 20220801, 20220802, 20220803, 20220804, 20220805 };
+		start_evaluate("evaluate.ini", "SHFE.rb2210", trading_days);
+	}
 	return 0;
 }
