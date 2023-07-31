edd381af91a729f50cdf8f2ccdc80a2c9c00693f
邹吉华 <ghuazo@live.com>
2023-07-31 10:48:03 +0800

    删除bin文件

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 APP_DIR=$(dirname $(readlink -f "$0"))
-export LD_LIBRARY_PATH=$APP_DIR../api/v6.6.9_traderapi_20220920/v6.6.9_20220914_api_linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$APP_DIR../api/CTP_V6.6.9_20220920/linux64:$LD_LIBRARY_PATH
 
 example runtime
